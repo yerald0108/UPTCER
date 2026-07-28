049049f1bbfb4f643f827d8e43d93a75445e7f4d
yerald0108 <yeraldfuste@gmail.com>
2026-07-28 11:12:10 -0400

    credeniciales.env

--- config/settings.py
+++ config/settings.py
@@ -133,23 +133,13 @@ else:
 # ------------------------------------------------------------------------------
 # EMAIL
 # ------------------------------------------------------------------------------
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
 EMAIL_HOST = "smtp.gmail.com"
-
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = ""
-
-EMAIL_HOST_PASSWORD = "wxpt wouy svvu ygxe"
-
-DEFAULT_FROM_EMAIL = (
-    "Sistema de Autorizaciones "
-    "<cristhianmr313@gmail.com>"
-)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 APP_NAME = "Sistema de Autorizaciones"
 
